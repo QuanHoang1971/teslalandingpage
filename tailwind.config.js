8d61de438ffa03e0274e8dbd7f503eda7fae4750
QuanHoang1971 <goodfriend3010@gmail.com>
2022-08-12 10:32:24 +0900

    commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        title: ["Sansita", "sans-serif"],
+        body: ["Lato", "sans - serif"],
+      },
+      colors: {
+        pri: "#081F4D",
+        sec: "#0083FF",
+        gen: "#384071",
+        "our-bg": "#D9e6ff",
+      },
+    },
+  },
+  plugins: [],
+};
